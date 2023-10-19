4ad48e6db502cb5c7d23e8378a8da65c7c2771de
unknown <admin@LAB3-30.bglab.local>
2023-10-19 20:53:23 -0300

    Suzana e Douglas

---
+++ bitcoin/lib/bitcoin.dart
@@ -0,0 +1,53 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+
+class Bitcoin extends StatefulWidget {
+  const Bitcoin({super.key});
+
+  @override
+  State<Bitcoin> createState() => _BitcoinState();
+}
+
+class _BitcoinState extends State<Bitcoin> {
+  String _preco = "0";
+
+  void _recuperarPreco() async {
+    var url = Uri.parse("http://blockchain.info/ticker");
+
+    http.Response response = await http.get(url);
+    Map<String, dynamic> retorno = json.decode(response.body);
+    setState(() {
+      _preco = retorno["BRL"] ["buy"].toString();
+    });
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(32.0),
+        child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset("imagens/bitcoin.png"),
+            const SizedBox(height: 30,),
+            Text("R\$ $_preco", style: TextStyle(fontSize: 35),),
+            const SizedBox(height: 30,),
+            SizedBox(height: 30,),
+            
+            TextButton(onPressed: (){_recuperarPreco();},
+              style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.orange)),
+              child: const Text("Atualizar", style: TextStyle(fontSize: 20, color: Colors.white),
+            ),
+            ),
+          ]
+          ),
+      ),
+    ),
+    );
+  }
+}
